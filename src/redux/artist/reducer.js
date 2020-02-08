25a13dfbe73d94702b9897dd5e7cd4846cc31b01
daddehs1 <sdaddeh2@gmail.com>
2020-02-07 16:30:06 -0800

    Setup Redux

---
+++ src/redux/artist/reducer.js
@@ -0,0 +1,40 @@
+// reducer
+// consume action and make appropriate mutations to state based on action type
+
+import {ADD_SELECTED_ARTIST, REMOVE_SELECTED_ARTIST} from './actionTypes';
+
+// define conditions for initial state
+const initialState = {
+  selectedArtistSpotifyIDs: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_SELECTED_ARTIST:
+      {
+        let {artistSpotifyID} = action.payload;
+        let artistAlreadySelected = state.selectedArtistSpotifyIDs.indexOf(artistSpotifyID) !== -1;
+        let newSelectedArtistSpotifyIDs = artistAlreadySelected
+          ? state.selectedArtistSpotifyIDs
+          : [
+            ...state.selectedArtistSpotifyIDs,
+            artistSpotifyID
+          ]
+        return {
+          ...state,
+          selectedArtistSpotifyIDs: newSelectedArtistSpotifyIDs
+        }
+      }
+    case REMOVE_SELECTED_ARTIST:
+      {
+        let {artistSpotifyID} = action.payload;
+        let newSelectedArtistSpotifyIDs = state.selectedArtistSpotifyIDs.filter(id => id !== artistSpotifyID);
+        return {
+          ...state,
+          selectedArtistSpotifyIDs: newSelectedArtistSpotifyIDs
+        }
+      }
+    default:
+      return state;
+  }
+}
