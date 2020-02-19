650f7b4b7218029d38f7ce576a613d0c4b540b9d
daddehs1 <sdaddeh2@gmail.com>
2020-02-19 00:23:04 -0800

    Fix typo in search reducer

--- src/redux/search/reducer.js
+++ src/redux/search/reducer.js
@@ -13,7 +13,7 @@ export default function(state = initialState, action) {
       {
         return {
           ...state,
-          searchFilter: action.payload.searchFilter
+          searchFilter: action.payload.filterString
         }
       }
     default:
